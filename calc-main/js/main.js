931b35b251fc1b749403235e0bf8fc64b10ab27e
unknown <otabek09@gmail.com>
2024-10-31 08:49:04 -0700

    finish

--- calc-main/js/main.js
+++ calc-main/js/main.js
@@ -7,35 +7,77 @@ const remove = document.querySelector('.r')
 const equal = document.querySelector('.equal')
 
 class Calculator {
+    signView
+    signOperator
+    operators = ['+','-','✕','÷']
+    get lastValue() {
+        return display.value[display.value.length - 1]
+    }
+
+    get firstValue() {
+        return display.value()
+    }
     setDisplay(value) {
         display.value = display.value + value
     }
     numbers(event) {
         let num = event.target.textContent
         this.setDisplay(num)
+
+        if (this.lastValue == 0 && display.value.length == 1)
+            return display.value = num
+        if (
+            this.lastValue == 0 && this.signView
+        ) return display.value = display.value.slice(0, -1) + num
+
+        this.setDisplay(num)
+        if (
+            !display.value ||
+            this.lastValue == "." ||
+            this.signOperator
+        ) return
     }
     signs(event) {
         const signView = event.target.textContent
+        const signOperator = event.target.dataset.sign
+       
+
+
+        this.signView = signView
         this.setDisplay(signView)
     }
 
     dot() {
         this.setDisplay('.')
+        if (
+            !display.value ||
+            this.operators.includes(this.lastValue)
+        ) {
+            display.value = display.value + '0.'
+        }
+        if (
+            !display.value ||
+            this.lastValue == "."
+        ) return
     }
     clear() {
         display.value = null
     }
 
     remove() {
-     let deleted = display.value.split("")
-     let newValue = deleted.slice(0,-1).join("")
+        let deleted = display.value.split("")
+        let newValue = deleted.slice(0, -1).join("")
 
-     display.value = newValue
+        display.value = newValue
     }
-   
+
 
     equal() {
-
+      const [num1, num2] = display.value.split(this.signView)
+      console.log(num1,num2)
+      display.value = eval(num1 + this.signOperator + num2)
+      this.signOperator = null
+      this.signView = null
     }
 }
 const calc = new Calculator()
@@ -56,10 +98,10 @@ dot.addEventListener('click', e => {
     calc.dot()
 })
 
-clear.addEventListener('click' , e => {
+clear.addEventListener('click', e => {
     calc.clear()
 })
 
-remove.addEventListener('click',e => {
+remove.addEventListener('click', e => {
     calc.remove()
 })
